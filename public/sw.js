0a786d7a4aa8c4bd659c7a6201b7da07a2ce1c6b
mbabazielroy <69889595+mbabazielroy@users.noreply.github.com>
2025-02-24 10:54:42 -0800

    commit

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+// Basic service worker that caches static assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/gaze.svg'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
